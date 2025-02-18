1dd6e0229445b616d990c4b7c7400d34da6a111c
Charles Naudet <cnaudet@nd.edu>
2025-02-17 23:00:55 -0500

    updated test_mesh.cpp

--- test/mesh/test_mesh.cpp
+++ test/mesh/test_mesh.cpp
@@ -1,21 +1,27 @@
 #include "mesh/Mesh.hpp"
 #include <iostream>
+#include <vector>
+#include <cuda_runtime.h>
 
 int main() {
-    // Initialize Kokkos
-    Kokkos::initialize();
-
     // Create a mesh object
     Mesh mesh("simple_mesh.txt");
 
+    // Get node data from device
+    double* d_nodes = mesh.get_nodes();
+    int num_nodes = mesh.get_num_nodes();
+
+    // Allocate host memory
+    std::vector<double> h_nodes(num_nodes * 2);
+    
+    // Copy data from device to host
+    cudaMemcpy(h_nodes.data(), d_nodes, num_nodes * 2 * sizeof(double), cudaMemcpyDeviceToHost);
+
     // Print node coordinates
-    auto nodes = mesh.get_nodes();
     std::cout << "Nodes:\n";
-    for (size_t i = 0; i < nodes.extent(0); ++i) {
-        std::cout << nodes(i, 0) << " " << nodes(i, 1) << "\n";
+    for (int i = 0; i < num_nodes; ++i) {
+        std::cout << h_nodes[i * 2] << " " << h_nodes[i * 2 + 1] << "\n";
     }
 
-    // Finalize Kokkos
-    Kokkos::finalize();
     return 0;
 }
